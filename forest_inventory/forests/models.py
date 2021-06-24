aaeb20be9f992eaf3274ffaf3a45d4430c8c5a71
Eric Sherman <ericandrewsherman@gmail.com>
2021-06-24 14:51:44 -0500

    init cookiecutter setup + backend model

---
+++ forest_inventory/forests/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+import enum
+
+from django.db.models.enums import TextChoices
+from django.db.models.fields import CharField, IntegerField, DecimalField, TextField
+from django.db.models.fields.files import ImageField
+
+
+class Forest(models.Model):
+    class ForestType(TextChoices):
+        CONSERVATION = "Conservation"
+        REFORESTATION = "Reforestation"
+        __empty__ = "Unknown"
+
+    name = CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=ForestType.choices)
+    image = ImageField()
+    latitude = DecimalField(max_digits=9, decimal_places=6)
+    longitude = DecimalField(max_digits=9, decimal_places=6)
+    area_covered = IntegerField()
+    country = CharField(max_length=255)
+    description = TextField()
+    carbon_stored = IntegerField()
+    change_in_carbon_stored_in_last_30_days = IntegerField()
