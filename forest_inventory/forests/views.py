bbec842ae18518066cef8360bd2d0aed1fd01b24
Eric Sherman <ericandrewsherman@gmail.com>
2021-06-24 16:24:16 -0500

    basic bootstrap pagination

--- forest_inventory/forests/views.py
+++ forest_inventory/forests/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from forests.models import Forest
 
 class ForestListView(ListView):
+    context_object_name = 'forests'
     model = Forest
+    paginate_by = 6
 
 class ForestDetailView(DetailView):
     model = Forest
