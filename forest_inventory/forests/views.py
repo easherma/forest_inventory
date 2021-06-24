8c3d0b6bc88d424c02ba346bdd42357d7e75a60c
Eric Sherman <ericandrewsherman@gmail.com>
2021-06-24 15:58:03 -0500

    very basic view/templates

--- forest_inventory/forests/views.py
+++ forest_inventory/forests/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from forests.models import Forest
 
 class ForestListView(ListView):
+    model = Forest
+
+class ForestDetailView(DetailView):
     model = Forest
